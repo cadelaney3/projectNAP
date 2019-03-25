92525ab33b7820b917999527a5c56b25ab1e76b7
Chris Delaney <cadelaney3@gmail.com>
2019-03-24 21:26:17 -0700

    quick change

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from wtforms.widgets import TextArea
-from flask_bootstrap import Bootstrap
 import requests
 import json
 import sys
